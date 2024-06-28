1613ff6a083dcc1ba784f935a3a52f1c1c8f6b1e
Juan Vasquez <juan@ombulabs.com>
2024-06-27 22:35:34 -0600

    fix(simple_form): Add simple_form initializer

--- lib/simple_form/theme.rb
+++ lib/simple_form/theme.rb
@@ -1,7 +1,12 @@
-require "simple_form/theme/version"
-require "simple_form/theme/engine"
+# frozen_string_literal: true
+
+require 'simple_form/theme/version'
+require 'simple_form/theme/engine'
+
+require 'simple_form'
 
 module SimpleForm
+  # SimpleFormTheme
   module Theme
     # Your code goes here...
   end
