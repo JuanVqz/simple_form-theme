0edb623f231d2ea090842aa9654f1b36692fb5e4
Juan Vasquez <juan@ombulabs.com>
2024-07-07 00:20:25 -0600

    fix(tailwindcss): Add required mark config
    This change is adding the required CSS color to the required fields.

--- lib/generators/simple_form/theme/tailwind/tailwind_generator.rb
+++ lib/generators/simple_form/theme/tailwind/tailwind_generator.rb
@@ -3,8 +3,27 @@
 class SimpleForm::Theme::TailwindGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
 
-  desc 'Copy the tailwindcss initializer file for simple_form'
-  def copy_initializer_file
+  desc 'Copy the tailwindcss files for simple_form'
+  def generate
+    install if install?
+  end
+
+  private
+
+  def install
+    copy_initializers
+    copy_locales
+  end
+
+  def install?
+    name == 'install'
+  end
+
+  def copy_initializers
     template 'config/initializers/simple_form_tailwindcss.rb', 'config/initializers/simple_form_tailwindcss.rb'
+  end
+
+  def copy_locales
+    directory 'config/locales'
   end
 end
--- test/lib/generators/simple_form/theme/simple_form/theme/tailwind_generator_test.rb
+++ test/lib/generators/simple_form/theme/simple_form/theme/tailwind_generator_test.rb
@@ -11,8 +11,18 @@ module SimpleForm::Theme
 
     test 'generator runs without errors' do
       assert_nothing_raised do
-        run_generator ['install']
+        run_generator ['random']
       end
+    end
+
+    test 'generator copies the initializer file' do
+      run_generator ['install']
+      assert_file 'config/initializers/simple_form_tailwindcss.rb'
+    end
+
+    test 'generator copies the locales files' do
+      run_generator ['install']
+      assert_directory 'config/locales'
     end
   end
 end
